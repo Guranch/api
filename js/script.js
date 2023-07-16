414ce8e7cc27a2b5ced5ea7731599c0006bbdbdd
Abramov <120345514+Guranch@users.noreply.github.com>
2023-07-16 17:05:51 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,29 +1,8 @@
 const pokemonName = document.querySelector('.pokemonname');
 const pokemonImg = document.querySelector('.pokemonimg');
-const pokemonImgContainer =document.querySelector('.img-container');
 const pokemonId = document.querySelector('.pokemonid');
 const pokemonTypes =document.querySelector('.pokemontypes');
 const pokemonStats = document.querySelector('.pokemonstats');
-
-const typeColors = {
-    electric: '#FFEA70',
-    normal: '#B09398',
-    fire: '#FF675C',
-    water: '#0596C7',
-    ice: '#AFEAFD',
-    rock: '#999799',
-    flying: '#7AE7C7',
-    grass: '#4A9681',
-    psychic: '#FFC6D9',
-    ghost: '#561D25',
-    bug: '#A2FAA3',
-    poison: '#795663',
-    ground: '#D2B074',
-    dragon: '#DA627D',
-    steel: '#1D8A99',
-    fighting: '#2F2F2F',
-    default: '#2A1A1F',
-};
 
 
 const searchPokemon = event => {
@@ -63,7 +42,6 @@ const renderPokemonTypes = types => {
     pokemonTypes.innerHTML = '';
     types.forEach(type => {
         const typeTextElement = document.createElement("div");
-        typeTextElement.style.color = typeColors[type.type.name];
         typeTextElement.textContent = type.type.name;
         pokemonTypes.appendChild(typeTextElement);
     });
